67aa485e5b338e55a01455d603d13e6838b17ec9
mrinal-mann <mrinal.mann24@gmail.com>
2024-11-24 19:17:57 +0530

    final site complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
+import Features from "./components/Features";
+import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
-const App = () => {
+function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
       <Hero />
-      <section className="z-0 min-h-screen bg-blue-500" />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   );
-};
+}
 
 export default App;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
   const [loadedVideos, setLoadedVideos] = useState(0);
 
   //
-  const totalVideos = 4;
+  const totalVideos = 3;
   const nextVideoRef = useRef(null);
   //
   const handleVideoLoad = () => {
